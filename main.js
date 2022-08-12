c627721e033a2f963d53017414fbdf2997965ecd
Airton Adams <68087823+AirtonAdams@users.noreply.github.com>
2022-08-12 17:12:34 -0300

    fix showNavOnScroll

--- main.js
+++ main.js
@@ -32,10 +32,11 @@ function activateManuAtCurrentSection(section) {
 }
 
 function showNavOnScroll() {
+  const nav = document.getElementById('navigation')
   if (scrollY > 0) {
-    navigation.classList.add('scroll')
+    nav.classList.add('scroll')
   } else {
-    navigation.classList.remove('scroll')
+    nav.classList.remove('scroll')
   }
 }
 function showBackToTopButtonOnScroll() {
